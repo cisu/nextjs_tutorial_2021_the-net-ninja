59f6cd31b3d202103a5cba64ccedb168506bff3e
cisu <49948070+cisu@users.noreply.github.com>
2021-04-28 00:04:41 +0300

    Pages & Routes

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor fugit sit quisquam nobis aliquam at molestias totam odit earum repellat tempora a accusantium, ipsa mollitia. Iure unde maxime sed amet quod hic dicta consequuntur at, quisquam a repellendus temporibus cum repudiandae voluptatum nemo dolorem voluptates libero harum dolore nulla ipsum?</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor fugit sit quisquam nobis aliquam at molestias totam odit earum repellat tempora a accusantium, ipsa mollitia. Iure unde maxime sed amet quod hic dicta consequuntur at, quisquam a repellendus temporibus cum repudiandae voluptatum nemo dolorem voluptates libero harum dolore nulla ipsum?</p>
+        </div>
+    )
+}
+
+export default About
