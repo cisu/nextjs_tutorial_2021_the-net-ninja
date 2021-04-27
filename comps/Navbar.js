2e8e0abfc4c07bfa09bdc8117ad7d19b5b03dea1
cisu <49948070+cisu@users.noreply.github.com>
2021-04-28 00:18:18 +0300

    Adding Other Components

---
+++ comps/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="logo">
+                <h1>Ninja List</h1>
+            </div>
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Ninja Listing</a>
+        </nav>
+    )
+}
+
+export default Navbar
+  
+
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import Head from 'next/head'
+import Footer from '../comps/Footer'
+import Navbar from '../comps/Navbar'
 import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Navbar />
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor fugit sit quisquam nobis aliquam at molestias totam odit earum repellat tempora a accusantium, ipsa mollitia. Iure unde maxime sed amet quod hic dicta consequuntur at, quisquam a repellendus temporibus cum repudiandae voluptatum nemo dolorem voluptates libero harum dolore nulla ipsum?</p>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor fugit sit quisquam nobis aliquam at molestias totam odit earum repellat tempora a accusantium, ipsa mollitia. Iure unde maxime sed amet quod hic dicta consequuntur at, quisquam a repellendus temporibus cum repudiandae voluptatum nemo dolorem voluptates libero harum dolore nulla ipsum?</p>
+      <Footer />
 
     </div>
   )
