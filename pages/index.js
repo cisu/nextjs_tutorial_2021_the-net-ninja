7dbd22884763975c4d7967c58e7324203f2bf62a
cisu <49948070+cisu@users.noreply.github.com>
2021-04-28 13:56:59 +0300

    Adding Styles

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,18 @@
 import Head from 'next/head'
-import Footer from '../comps/Footer'
-import Navbar from '../comps/Navbar'
+
 import styles from '../styles/Home.module.scss'
 import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
    
-      <h1>Homepage</h1>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor fugit sit quisquam nobis aliquam at molestias totam odit earum repellat tempora a accusantium, ipsa mollitia. Iure unde maxime sed amet quod hic dicta consequuntur at, quisquam a repellendus temporibus cum repudiandae voluptatum nemo dolorem voluptates libero harum dolore nulla ipsum?</p>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor fugit sit quisquam nobis aliquam at molestias totam odit earum repellat tempora a accusantium, ipsa mollitia. Iure unde maxime sed amet quod hic dicta consequuntur at, quisquam a repellendus temporibus cum repudiandae voluptatum nemo dolorem voluptates libero harum dolore nulla ipsum?</p>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor fugit sit quisquam nobis aliquam at molestias totam odit earum repellat tempora a accusantium, ipsa mollitia. Iure unde maxime sed amet quod hic dicta consequuntur at, quisquam a repellendus temporibus cum repudiandae voluptatum nemo dolorem voluptates libero harum dolore nulla ipsum?</p>
+      <p className={styles.text}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor fugit sit quisquam nobis aliquam at molestias totam odit earum repellat tempora a accusantium, ipsa mollitia. Iure unde maxime sed amet quod hic dicta consequuntur at, quisquam a repellendus temporibus cum repudiandae voluptatum nemo dolorem voluptates libero harum dolore nulla ipsum?</p>
       
       <Link href="/ninjas">
-        <a href="">See Ninja Listing</a>
+        <a className={styles.btn} href="">See Ninja Listing</a>
       </Link>
     
 
