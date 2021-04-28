e20c77660c8be4056c492827c15e244e67397755
cisu <49948070+cisu@users.noreply.github.com>
2021-04-28 18:05:59 +0300

    Dynamic Routes (part 2 - getStaticPaths)

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,5 +1,23 @@
 import React from 'react'
 
+export const getStaticPaths = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = res.json();
+
+    const paths = data.map(ninja => {
+        return {
+            params: { id: ninja.id.toString()}
+        }
+    })
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+
+
 const Details = () => {
     return (
         <div>
