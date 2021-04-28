d2077228e5944d8048e7e5d5cd7cb2447ba5fa2a
cisu <49948070+cisu@users.noreply.github.com>
2021-04-28 18:23:55 +0300

    Fetching a Single Item

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const getStaticPaths = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/users')
-    const data = res.json();
+    const data = await res.json();
 
     const paths = data.map(ninja => {
         return {
@@ -16,12 +16,27 @@ export const getStaticPaths = async () => {
     }
 }
 
+export const getStaticProps = async (context) => {
+    const id = context.params.id;
+
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+
+    const data = await res.json();
+
+    return {
+        props: { ninja: data }
+    }
+}
 
 
-const Details = () => {
+
+const Details = ({ninja}) => {
     return (
         <div>
-            <h1>Details</h1>
+            <h1>{ninja.name}</h1>
+            <p>{ninja.email}</p>
+            <p>{ninja.website}</p>
+            <p>{ninja.address.city}</p>
         </div>
     )
 }
